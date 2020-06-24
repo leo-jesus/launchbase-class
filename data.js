6898b3a82ea8fc5ff0ed83fc3b279870109c9aaa
leo-jesus <llxdgb1@gmail.com>
2020-06-24 16:14:24 -0300

    atualizar os arquivos com informações de cursos e descrição de forma dinâmica

---
+++ data.js
@@ -0,0 +1,45 @@
+module.exports = [
+    {
+        id:"GykTLqODQuU",
+        title:"Formulário Animado com JS puro e CSS | Mayk Brito",
+        duration:"57 min",
+        price:"FREE"
+
+    },
+    {
+        id:"vqrjFnq3-uo",
+        title:"Criando Player de Audio | Mayk Brito",
+        duration:"105 min",
+        price:"FREE"
+
+    }, 
+    {
+        id:"GSqR2i-Pq6o",
+        title:"Desvendando a variável THIS | Mayk Brito",
+        duration:"54 min",
+        price:"FREE"
+
+    }, 
+    {
+        id:"rAzHvYnQ8DY",
+        title:"Array: High Order Functions | Mayk Brito",
+        duration:"57 min",
+        price:"FREE"
+
+    }, 
+    {
+        id:"2alg7MQ6_sI",
+        title:"Git e Github na Prática | Mayk Brito",
+        duration:"57 min",
+        price:"FREE"
+
+    }, 
+    {
+        id:"HN1UjzRSdBk",
+        title:"Desvendando o CSS Grid na Prática | Mayk Brito",
+        duration:"57 min",
+        price:"FREE"
+
+    }
+    
+]
--- server.js
+++ server.js
@@ -2,20 +2,34 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data")
 
 server.use(express.static('public'))
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express:server
+    express: server, 
+    autoescape:false
 })
 
-server.get("/", (req, res)=>{
-    return res.render("about")
+server.get("/", (req, res) => {
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/21048049?s=400&u=235dd059e0607a85e18522b6c906fef968dac12d&v=4",
+        name: "Leonardo Jesus",
+        role: "Aluno - Rocketseat",
+        description: 'Futuro programador full-stack. Servidor no <a href="http://www.tre-sp.jus.br" target="_blank">TRE-SP</a>',
+        links: [
+            { name: "Github", url: "https://github.com/leo-jesus/" },
+            { name: "Twitter", url: "https://www.twitter.com/llxdgb1" },
+            { name: "Linkedin", url: "https://www.linkedin.com/in/leonardo-jesus-226a97112/" }
+        ]
+    }
+
+    return res.render("about", { about })
 })
-server.get("/portfolio", (req, res)=>{
-    return res.render("portfolio")
+server.get("/portfolio", (req, res) => {
+    return res.render("portfolio", { items: videos })
 })
-server.listen(5000, ()=>{
+server.listen(5000, () => {
     console.log("server is running")
 }) 
