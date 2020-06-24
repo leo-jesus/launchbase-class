e2f2b7d0b1b352d448237dfc00d71375fdb15a19
leo-jesus <llxdgb1@gmail.com>
2020-06-24 14:30:14 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+server.use(express.static('public'))
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.get("/", (req, res)=>{
+    return res.render("about")
+})
+server.get("/portfolio", (req, res)=>{
+    return res.render("portfolio")
+})
+server.listen(5000, ()=>{
+    console.log("server is running")
+}) 
